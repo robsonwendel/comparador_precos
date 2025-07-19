efa567a3bb798c12ff41a7dad8a0e8b4d43a88f6
robsonwendel <robsonwendel@gmail.com>
2025-07-19 14:33:22 -0300

    endereço

--- frontend/compras.js
+++ frontend/compras.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const apiBaseUrl = 'http://127.0.0.1:5000/api';
+    const apiBaseUrl = 'https://comparador-api.onrender.com/api';
 
     const searchInput = document.getElementById('produtoSearch');
     const autocompleteResults = document.getElementById('autocompleteResults');
